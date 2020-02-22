5f9e9a98b8ce91d363177366ce775f677d374d27
Marr11317 <remimarche@gmail.com>
2020-02-21 20:06:27 -0500

    .h works

--- cmdPython.py
+++ cmdPython.py
@@ -2,15 +2,35 @@ originalfile = open("shortcut.cpp", "r")
 text = ""
 while True :
     line = originalfile.readline()
-    if line.strip().startswith('"') and not ' ' in line.strip() and not line.strip().replace('",', '').replace('"', '').upper() in text: 
-        text += '      ' + line.strip().replace('",', '').replace('"', '').upper() + ',\n'
+    value = line.strip().replace(',', '').replace('"', '').replace('-', '_').upper()
 
     if line == "/// END OF FILE\n" :
         break
 
-#print(text)
-moldFile = open('mold.h', 'r')
-outputFile = open("cmdlist.h", "w+")
+    if (line.strip().startswith('"')    # check if it is a string
+        and not ' ' in line.strip()     # check if it doesn't contain spaces
+        and not value in text           # be sure it's not there twice
+        and not value.replace('_', '') in text) :  # some values like RELAYOUT also come as RE_LAYOUT   
 
-mold = moldFile.read()
-outputFile.write(mold.replace('\\Replace', text))
+        text += '      ' + value + '\n'
+
+
+#print(text)
+
+# Generate the enum in the .h
+moldHFile = open('mold.h', 'r')
+outputHFile = open("cmdlist.h", "w+")
+
+mold = moldHFile.read()
+outputHFile.write(mold.replace('//Replace', text))
+
+moldHFile.close()
+outputHFile.close()
+
+# Generate the mapping to a String in the .cpp
+moldCppFile = open('mold.cpp', 'r')
+outputCppFile = open('cmdlist.cpp', 'w+')
+moldCpp = moldCppFile.read()
+
+moldCppFile.close()
+outputCppFile.close()
--- mold.cpp
+++ mold.cpp
@@ -2,24 +2,14 @@
 //  MuseScore
 //  Music Composition & Notation
 //
-//  Copyright (C) 2013 Werner Schweer
+//  Copyright (C) 2008-2020
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License version 2
 //  as published by the Free Software Foundation and appearing in
 //  the file LICENCE.GPL
 //=============================================================================
-#ifndef __CMDLIST_H__
-#define __CMDLIST_H__
 
 namespace Ms {
 
-enum class cmdList : unsigned int {
-      Invalid = 0,
-
-//Replace
-      };
-
-static QString 
-} // Ms
-#endif // __CMDLIST_H__
+} // namespace Ms
